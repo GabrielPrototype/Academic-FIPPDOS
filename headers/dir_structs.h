5ecbd63ecb184d5df30e25007b6e055cef781d23
Aleksander <ale_timao06@hotmail.com>
2017-03-19 00:25:29 -0300

    diretorio

--- headers/dir_structs.h
+++ headers/dir_structs.h
@@ -15,28 +15,64 @@
 #define DIR_STRUCTS_H
 
 #include <stddef.h>
+#include <string.h>
 #include "file_structs.h"
 
-struct dir_header{
+struct dir_header {
     File_header *ListaArq;
-    struct dir_header *Head, *PtrPai, *Tail;
+    struct dir_header *Head, *PtrPai, *Tail; //subdiretorio,ponteiropai,ponteiroirmao
     char NomeDir[255];
     char data[9];
     char hora[5];
-    
+
 };
 
 typedef struct dir_header Dir_header;
 
-void init_dir_header(Dir_header **PtrDir){
-    
-    (*PtrDir) = (Dir_header*) malloc(sizeof(Dir_header));
+void init_dir_header(Dir_header **PtrDir) {
+
+    (*PtrDir) = (Dir_header*) malloc(sizeof (Dir_header));
     (*PtrDir)->Head = NULL;
     (*PtrDir)->Tail = NULL;
     (*PtrDir)->ListaArq = NULL;
     (*PtrDir)->PtrPai = NULL;
+    strcpy((*PtrDir)->NomeDir[0], "nome");
     (*PtrDir)->data[0] = '\n';
     (*PtrDir)->hora[0] = '\n';
 };
+
+dir_header *Cons(Dir_header **Construcao) {
+
+}
+
+void insere_diretorio(Dir_header **Pai, Dir_header **Diretorio) {
+    Dir_header *aux, *ant;
+    //verificar se e a unica pasta dentro do pai se nao inserir ordenado
+    if ((*Pai)->Head == NULL) {
+        (*Pai)->Head = (*Diretorio);
+        (*Diretorio)->PtrPai = (*Pai);
+    } else {
+        if (strcmp((*Diretorio)->NomeDir ,(*Pai)->Head->NomeDir) < 0) {
+            (*Diretorio)->Tail = (*Pai)->Head;
+            (*Pai)->Head = (*Diretorio); 
+        } else {
+            aux = (*Pai)->Head;
+            while (aux != NULL && strcmp(aux->NomeDir, aux->Tail->NomeDir) >= 0) {
+                ant = aux;
+                aux = aux->Tail;
+            }
+            if (strcmp(aux->NomeDir, (*Diretorio)->NomeDir) == 0) {
+                return -1;
+            }
+            if (aux->Tail == NULL) {
+                ant->Tail = Diretorio;
+            }
+        }
+    }
+}
+
+Dir_header procurar_pasta(Dir_header **Pai, char *nomePasta) {
+
+}
 #endif /* DIR_STRUCTS_H */
 
