4cd004737538af1a8f67f4cb203a6500beb86293
Gabriel L. P. Abreu <gabriel.longo.longo@gmail.com>
2017-04-10 00:19:00 -0300

    Adicionado error_codes.h para numerar e atribuir constantes para erros especificos.

---
+++ headers/errors_codes.h
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   errors_codes.h
+ * Author: titan
+ *
+ * Created on 10 de Abril de 2017, 00:10
+ */
+
+#ifndef ERRORS_CODES_H
+#define ERRORS_CODES_H
+
+#define ERROR_DIR_NOT_FOUND -301;
+#define ERROR_DUPLICATED_DIR -302;
+
+#define ERROR_FILE_NOT_FOUND -311;
+#define ERROR_DUPLICATED_FILE -312
+
+#endif /* ERRORS_CODES_H */
+
